a37547ad8692798a39873f582ef0712867afe029
Monalisa Onyango <+>
2024-09-03 00:01:35 +0300

    inheritance2

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,31 @@
 class Student:
-    pass
+
+    def raise_hand(self):
+        print("Pick me!") 
+
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+
 
 class ChattyStudent(Student):
-    pass
+
+    def raise_hand(self):
+        for _ in range(10):
+          super().raise_hand()
+
+    def hello(self):
+        super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+  
+    
+
+student = Student()
+student.raise_hand()
+student.hello()
+
+
+kichwa_ngumu = ChattyStudent()
+kichwa_ngumu.raise_hand()
+kichwa_ngumu.hello()
+
